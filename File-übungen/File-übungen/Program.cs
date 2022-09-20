a371129b6e733f215ebb83383e2b71cf71066c64
Mohr, Robin <robin.mohr@tecalliance.net>
2022-09-20 15:19:23 +0200

    Training with files and csv

---
+++ File-übungen/File-übungen/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_übungen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
+            ////fi.CopyTo("C:/010Pojects/014Uebungen2/File-übungen/meine_datei123.csv");
+
+            ////FileInfo fi = new FileInfo("C:/010Pojects/014Uebungen2/File-übungen/meine_datei123.csv");
+            ////fi.Delete();
+
+            //Console.WriteLine("Geb mir den Pfad an, wohin die Datei geschrieben werden soll!");
+            //string path = Console.ReadLine();
+
+            //DirectoryInfo di = new DirectoryInfo(path);
+
+            //while (di.Exists == false)
+            //{
+            //    Console.WriteLine("Dein Pfad existiert nicht, gib mir einen neuen!");
+            //    path = Console.ReadLine();
+            //    di = new DirectoryInfo(path);
+            //}
+
+            //path.Replace("\\", "\\\\");
+
+            //Console.WriteLine("Wie soll die Datei heißen?");
+
+            //string filename = Console.ReadLine();
+
+
+
+
+            //fi.CopyTo(path + "\\" + filename);
+
+            //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMIS-data.csv");
+
+            //if (fi.Exists)
+            //{
+            //    fi.CopyTo("C:\\010Pojects\\014Uebungen2\\File - übungen");
+
+            //}
+
+            string path = @"C:\010Pojects\014Uebungen2\File-übungen\bizzii.csv";
+
+            StreamReader sr = new StreamReader(path);
+            List<string[]> einträge = new List<string[]>();
+            //Remove Header
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                einträge.Add(sr.ReadLine().Split(';'));                
+            }
+            
+            for (int age = 18; age < 101; age++)
+            {
+                using (StreamWriter sw = new StreamWriter($@"C:\010Pojects\014Uebungen2\File-übungen\Gewichte_{age}.csv"))
+                {
+
+                    for (int i = 0; i < einträge.Count; i++)
+                    {
+                        if (Convert.ToInt32(einträge[i][2]) == age)
+                        {
+                            sw.WriteLine(string.Join(";", einträge[i]));
+                            Console.WriteLine(string.Join("\t", einträge[i]));
+                        }
+                    }
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
--- Uebungen2/ConsoleApp1/Program.cs
+++ Uebungen2/ConsoleApp1/Program.cs
@@ -12,116 +12,50 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Seite 3 Aufgabe 1
-            do
+            //Seite 4 Aufgabe 1
+
+            Console.WriteLine("Wie viele Einträge soll das Array haben?");
+
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            double[] numbers = new double[i];
+
+            double sum1 = 0;
+            double sum2 = 0;
+            double sum3 = 0;
+
+            int counter = 0;
+
+            Console.Clear();
+
+            for (int j = 0; j < i; j++)
             {
-                Console.Clear();
-
-                Console.WriteLine("Wie ist dein Name?");
-                string name = Console.ReadLine();
-
-                if (name == " ")
-                {
-                    Console.WriteLine("Hast du keinen Namen?");
-                    return;
-                }
-                else if (name != " ")
-                {
-                    Console.WriteLine("Willst du das als deinen Namen festlegen?(y/n)");
-                    string auswahl = Console.ReadLine();
-
-                    if (auswahl == "y")
-                    {
-                        string[] splitted = name.Split('a');
-
-                        Console.WriteLine($"Dein Name hat {splitted.Length - 1} a's.");
-
-                        Console.WriteLine("Gib mir bitte 2 Zahlen nacheinander");
-                        int num1 = Convert.ToInt32(Console.ReadLine());
-                        int num2 = Convert.ToInt32(Console.ReadLine());
-
-                        if (num1 < num2)
-                        {
-                            Console.WriteLine($"Von deinen Ausgewählten Zahlen ist die {num2} Größer.");
-                            break;
-                        }
-                        else if (num1 == num2)
-                        {
-                            Console.WriteLine("Deine Ausgewählten Zahlen sind gleich groß");
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Von deinen Ausgewählten Zahlen ist die {num1} Größer.");
-                            break;
-                        }
-                    }
-                }
-
-                Console.ReadKey();
-
-            } while (true);
-            
-            Console.ReadKey();
-
-            //Seite 3 Aufgabe 2
-
-            Console.WriteLine("Bitte gib eine Anzahl von sekunde an, ich werde diese dann in (d,h,m,s) umwandeln.");
-
-            int seconds = Convert.ToInt32(Console.ReadLine());
-
-            int minutes = seconds / 60;
-            int a_seconds = seconds % 60;
-
-            int hours = minutes / 60;
-            int a_minutes = minutes % 60;
-
-            int days = hours / 60;
-            int a_hours = hours % 60;
-                        
-            int a_days = days % 60;
-
-            Console.WriteLine($"Umgewandelt sind es {a_days} Tage, {a_hours} Stunden, {a_minutes} Minuten und {a_seconds} Sekunden.");
-
-            Console.ReadKey();
-
-            //Seite 3 Aufgabe 3
-
-            string text1 = "MissiSsiPpi";
-
-            int m = 0;
-            int i = 0;
-            int s = 0;
-            int p = 0;
-            
-            foreach (char c in text1.ToLower())
-            {
-                if (c == 'm')
-                {
-                    m++;
-                }
-
-                else if (c == 'i')
-                {
-                    i++;
-                }
-
-                else if (c == 's')
-                {
-                    s++;
-                }
-
-                else if (c == 'p')
-                {
-                    p++;
-                }
+                Console.WriteLine($"Bitte gib mir die {j +1}. Zhl");
+                numbers[j] = Convert.ToDouble(Console.ReadLine());
             }
 
-            Console.WriteLine($"Das Wort Mississippi hat {m} m's, {i} i's, {s} s's und {p} p's.");
+            for (int j = 0; j < i; j++)
+            {
+                sum1 += numbers[j];
+            }
+
+            foreach (double j in numbers)
+            {
+                sum2 += j;
+            }
+
+            while (counter < i)
+            {
+                sum3 += numbers[counter];
+                counter++;
+            }
+            Console.Clear();
+
+            Console.WriteLine($"Die Lösung der for-Schleife ist {sum1}. " + 
+                $"Die Lösung der foreach-Schleife ist {sum2}. " + 
+                $"Die Lösung der while-Schleife ist {sum3}.");
 
             Console.ReadKey();
-
-            
 
         }
     }
