0bd568f6f1d1add1b42285c95cc35fa1320e976c
Mohr, Robin <robin.mohr@tecalliance.net>
2022-09-21 11:27:32 +0200

    no message

--- Uebungen2/ConsoleApp1/Program.cs
+++ Uebungen2/ConsoleApp1/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,51 +15,168 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Seite 4 Aufgabe 1
+            ////Seite 4 Aufgabe 1
 
-            Console.WriteLine("Wie viele Einträge soll das Array haben?");
+            //Console.WriteLine("Wie viele Einträge soll das Array haben?");
 
-            int i = Convert.ToInt32(Console.ReadLine());
+            //int i = Convert.ToInt32(Console.ReadLine());
 
-            double[] numbers = new double[i];
+            //double[] numbers = new double[i];
 
-            double sum1 = 0;
-            double sum2 = 0;
-            double sum3 = 0;
+            //double sum1 = 0;
+            //double sum2 = 0;
+            //double sum3 = 0;
 
-            int counter = 0;
+            //int counter = 0;
 
-            Console.Clear();
+            //Console.Clear();
 
-            for (int j = 0; j < i; j++)
+            //for (int j = 0; j < i; j++)
+            //{
+            //    Console.WriteLine($"Bitte gib mir die {j +1}. Zhl");
+            //    numbers[j] = Convert.ToDouble(Console.ReadLine());
+            //}
+
+            //for (int j = 0; j < i; j++)
+            //{
+            //    sum1 += numbers[j];
+            //}
+
+            //foreach (double j in numbers)
+            //{
+            //    sum2 += j;
+            //}
+
+            //while (counter < i)
+            //{
+            //    sum3 += numbers[counter];
+            //    counter++;
+            //}
+            //Console.Clear();
+
+            //Console.WriteLine($"Die Lösung der for-Schleife ist {sum1}. " + 
+            //    $"Die Lösung der foreach-Schleife ist {sum2}. " + 
+            //    $"Die Lösung der while-Schleife ist {sum3}.");
+
+            //Console.ReadKey();
+            bool taschenrechner = true;
+
+
+            do
             {
-                Console.WriteLine($"Bitte gib mir die {j +1}. Zhl");
-                numbers[j] = Convert.ToDouble(Console.ReadLine());
-            }
+                string summe = Summe();
 
-            for (int j = 0; j < i; j++)
-            {
-                sum1 += numbers[j];
-            }
+                if (summe.StartsWith("n"))
+                {
+                    taschenrechner = false;
+                }
 
-            foreach (double j in numbers)
-            {
-                sum2 += j;
-            }
+                string endsumme = summe.TrimStart('n');
 
-            while (counter < i)
-            {
-                sum3 += numbers[counter];
-                counter++;
-            }
-            Console.Clear();
+                Console.WriteLine(endsumme);
 
-            Console.WriteLine($"Die Lösung der for-Schleife ist {sum1}. " + 
-                $"Die Lösung der foreach-Schleife ist {sum2}. " + 
-                $"Die Lösung der while-Schleife ist {sum3}.");
 
+            } while (taschenrechner);
             Console.ReadKey();
+            
 
+
+            
+            
+
+
+
+        }
+        static string Summe()
+        {
+            bool error = false;
+            double sum = 0;
+            string summ = "";
+            
+
+            
+                Console.WriteLine("Gebe mir eine Zahl");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Gebe mir eine zweite Zahl");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Was möchtest du mit den beiden Zahlen machen?( + , - , * , / , % , avg ");
+                string input = Console.ReadLine();
+
+
+                switch (input)
+                {
+                    case "+":
+                        Console.Clear();
+                        sum = num1 + num2;                        
+                        break;
+
+                    case "-":
+                        Console.Clear();
+                        sum = num1 - num2;
+                        break;
+
+                    case "*":
+                        Console.Clear();
+                        sum = num1 * num2;
+                        break;
+
+                    case "/":
+                        Console.Clear();
+                        sum = num1 / num2;
+                        break;
+
+                    case "%":
+                        Console.Clear();
+                        sum = num1 % num2;
+                        break;
+
+                    case "avg":
+                        Console.Clear();
+                        sum = (num1 + num2) / 2;
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Bitte gib einen validen operator ein.");
+                        error = true;
+
+                        break;
+                }
+                if (!error)
+                {
+                    if (input != "avg")
+                    {
+                        summ = $"{num1} {input} {num2} = {sum}";
+
+                        Console.ReadKey();
+                        Console.Clear();
+
+                        Console.WriteLine("Möchtest du noch etwas berechnen? (y/n)");
+                        string another = Console.ReadLine();
+
+                        if (another == "n")
+                        { 
+                            summ = $"n{summ}";
+                        }
+                    }
+                    else
+                    {
+                        summ = $" Der Durchschnitt von {num1} und {num2} ist {sum}";
+
+                        Console.ReadKey();
+                        Console.Clear();
+
+                        Console.WriteLine("Möchtest du noch etwas berechnen? (y/n)");
+                        string another = Console.ReadLine();
+
+                        if (another == "n")
+                        {
+                            summ = $"n{summ}";
+                        }
+                    }
+                }
+            return summ;
         }
     }
 }
